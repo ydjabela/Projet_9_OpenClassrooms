ee1a30c0204df8647ec5c42332d3c7a0b979d711
ydjabela <ydjabela@blendbow.com>
2022-05-18 12:31:01 +0200

    update

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,22 +1,11 @@
 # ~/projects/django-web-app/merchex/listings/views.py
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render
 from listings.models import Band
 
 
 def hello(request):
     bands = Band.objects.all()
+    return render(request, 'listings/hello.html', {'bands': bands})
 
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-    """)
-    '''
-    return render(request, 'listings/templates/listings/hello.html')
-    '''
